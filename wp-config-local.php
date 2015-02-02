271e6e156dae451f4db6b13f9dabf7a2d1d13e68
Kyle Samson <kylesamson21@gmail.com>
2015-02-02 13:56:23 -0400

    initial wordpress install

---
+++ wp-config-local.php
@@ -0,0 +1,21 @@
+<?php
+    // ** MySQL settings - You can get this info from your web host ** //
+    /** The name of the database for WordPress */
+    define('DB_NAME', 'northumberlamb');
+
+    /** MySQL database username */
+    define('DB_USER', 'northumberlamb');
+
+    /** MySQL database password */
+    define('DB_PASSWORD', 'lambchop15');
+
+    /** MySQL hostname */
+    define('DB_HOST', 'localhost');
+
+    /** Database Charset to use in creating database tables. */
+    define('DB_CHARSET', 'utf8');
+
+    /** The Database Collate type. Don't change this if in doubt. */
+    define('DB_COLLATE', '');
+
+	define('LOCAL_DEV', false);
