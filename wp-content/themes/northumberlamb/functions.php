f7dad99dbc09738eb1914c062d100ba004d86acf
Kyle Samson <kylesamson21@gmail.com>
2015-02-09 15:18:23 -0400

    created files and directories for custom site layout

--- wp-content/themes/northumberlamb/functions.php
+++ wp-content/themes/northumberlamb/functions.php
@@ -1,9 +1,36 @@
 <?php
-
+// ------ MENUS ------
+add_action( 'init', 'create_menus' );
+function create_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+    )
+  );
+}
+// ------ CUSTOM POST TYPES ------
 add_action( 'init', 'create_post_type' );
-
 function create_post_type() {
-
+    register_post_type( 'banner',
+        array(
+            'labels' => array(
+                'name' => 'Banner',
+                'singular_name' => '',
+            ),
+        'public' => true,
+        'has_archive' => true,
+        )
+    );
+    register_post_type( 'important-links',
+        array(
+            'labels' => array(
+                'name' => 'Important Links',
+                'singular_name' => 'Important Link',
+            ),
+        'public' => true,
+        'has_archive' => true,
+        )
+    );
     register_post_type( 'recipe',
         array(
             'labels' => array(
@@ -14,4 +41,24 @@ function create_post_type() {
         'has_archive' => true,
         )
     );
+     register_post_type( 'recipe-links',
+        array(
+            'labels' => array(
+                'name' => 'Recipe Links',
+                'singular_name' => 'Recipe Link',
+            ),
+        'public' => true,
+        'has_archive' => true,
+        )
+    );
+    register_post_type( 'clients',
+        array(
+            'labels' => array(
+                'name' => 'Client',
+                'singular_name' => 'Clients',
+            ),
+        'public' => true,
+        'has_archive' => true,
+        )
+    );    
 };
--- wp-content/themes/northumberlamb/page-recipe.php
+++ wp-content/themes/northumberlamb/page-recipe.php
@@ -1,4 +1,4 @@
 <?php
 /*
-Template Name: Recipe Page
+Template Name: Recipes Page
 */
