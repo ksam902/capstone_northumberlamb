6e0105587d697d4f3efedad832286768f9ede642
Kyle Samson <kylesamson21@gmail.com>
2015-02-11 11:36:56 -0400

    fixed htaccess issue that was preventing the navigation from working properly

---
+++ wp-content/themes/northumberlamb/templates_c/02c97a2dc7980d75fd734a2a9cadf52ac08ab9e0.file.content.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2015-02-11 15:31:16
+         compiled from "/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/consumers/content.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:33396914054db75c432ed81-13430795%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '02c97a2dc7980d75fd734a2a9cadf52ac08ab9e0' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/consumers/content.tpl',
+      1 => 1423512189,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '33396914054db75c432ed81-13430795',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_54db75c4377062_88835870',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54db75c4377062_88835870')) {function content_54db75c4377062_88835870($_smarty_tpl) {?><div>CONSUMERS PAGEEEEEEEEEEE</div><?php }} ?>
---
+++ wp-content/themes/northumberlamb/templates_c/dce18736a4cbf313c512f8de91a117b16f13c318.file.content.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2015-02-11 15:31:07
+         compiled from "/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/recipes/content.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:80731179154db75bb5d8d18-65229018%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'dce18736a4cbf313c512f8de91a117b16f13c318' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/recipes/content.tpl',
+      1 => 1423512071,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '80731179154db75bb5d8d18-65229018',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_54db75bb701b91_81159689',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54db75bb701b91_81159689')) {function content_54db75bb701b91_81159689($_smarty_tpl) {?><div>RECIPES PAGEEEEEEEEEEE</div><?php }} ?>
