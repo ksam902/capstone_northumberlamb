ed1d5277151c418468b61ac624bf60f20a0a7580
Kyle Samson <kylesamson21@gmail.com>
2015-02-20 13:58:07 -0400

    test push for template c folder

--- wp-content/themes/northumberlamb/page-recipe.php
+++ wp-content/themes/northumberlamb/page-recipe.php
@@ -4,7 +4,15 @@ Template Name: Recipes Page
 */
 $smarty = wp_smarty();
 $id = get_the_ID();
-
+$recipes = new WP_Query(array( 'post_type' => 'recipe', 'posts_per_page' => '-1'));
+$posts = array(
+	'recipes' => array(),
+);
+foreach ($recipes->posts as $p) {
+	$post = fetch($p);
+	$posts['recipes'] = $post;
+}
+pre($posts['recipes']);
 $content = array(
 	'page' => fetch_page('recipes'),
 );
