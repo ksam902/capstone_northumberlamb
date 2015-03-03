838325716f5de9fd2384e3406e42a07dfc597967
Kyle Samson <kylesamson21@gmail.com>
2015-03-03 09:57:05 -0400

    styling for the shippers page created

--- wp-content/themes/northumberlamb/templates/shippers/content.tpl
+++ wp-content/themes/northumberlamb/templates/shippers/content.tpl
@@ -1,12 +1,70 @@
 <div class="container">
 
     <div class="row">
-        <h2>
-            <strong>Shippers</strong>
-        </h2>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><strong>Become a Shipper</strong></h2>
+                    <div class="application-div box">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="centered">
+                            <button class="btn btn-default" type="submit">Shipper Application</button>
+                        </div>
+                    </div>
+                </div>
+              <div class="col-md-6">
+                    <h2><strong>Become a Member</strong></h2>
+                    <div class="application-div box">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="centered">
+                            <button class="btn btn-default" type="submit">Member Application</button>
+                        </div>
+                    </div>
+              </div>    
+            </div>
+        </div>
+        <h2><strong>Shippers</strong></h2>
         <div class="box">
             <div class="col-lg-12">
                 <p>{$content.page.post_content}</p>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <h2><strong>Lamb Grading Chart</strong></h2>
+            <div class="row box">
+                <div class="col-md-12 .table-responsive">
+                    <table class="table table-hover">
+                        <tr>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- wp-content/themes/northumberlamb/templates_c/dca0923b508dc52a48da3644b1bc0e4453e674e0.file.content.tpl.php
+++ wp-content/themes/northumberlamb/templates_c/dca0923b508dc52a48da3644b1bc0e4453e674e0.file.content.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.18, created on 2015-02-18 15:36:05
+<?php /* Smarty version Smarty-3.1.18, created on 2015-03-03 13:54:27
          compiled from "/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/shippers/content.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:212581338854e4b0ffd5c4a8-34523584%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'dca0923b508dc52a48da3644b1bc0e4453e674e0' => 
     array (
       0 => '/Applications/XAMPP/xamppfiles/htdocs/northumberlamb/wp-content/themes/northumberlamb/templates/shippers/content.tpl',
-      1 => 1424273764,
+      1 => 1425390864,
       2 => 'file',
     ),
   ),
@@ -26,13 +26,71 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <?php if ($_valid && !is_callable('content_54e4b0ffd9be04_22161936')) {function content_54e4b0ffd9be04_22161936($_smarty_tpl) {?><div class="container">
 
     <div class="row">
-        <h2>
-            <strong>Shippers</strong>
-        </h2>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><strong>Become a Shipper</strong></h2>
+                    <div class="application-div box">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="centered">
+                            <button class="btn btn-default" type="submit">Shipper Application</button>
+                        </div>
+                    </div>
+                </div>
+              <div class="col-md-6">
+                    <h2><strong>Become a Member</strong></h2>
+                    <div class="application-div box">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="centered">
+                            <button class="btn btn-default" type="submit">Member Application</button>
+                        </div>
+                    </div>
+              </div>    
+            </div>
+        </div>
+        <h2><strong>Shippers</strong></h2>
         <div class="box">
             <div class="col-lg-12">
                 <p><?php echo $_smarty_tpl->tpl_vars['content']->value['page']['post_content'];?>
 </p>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <h2><strong>Lamb Grading Chart</strong></h2>
+            <div class="row box">
+                <div class="col-md-12 .table-responsive">
+                    <table class="table table-hover">
+                        <tr>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                          <td class="info"><h6>Lorem Ipsum</h6></td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                        <tr>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                          <td class="">Lorem Ipsum</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
