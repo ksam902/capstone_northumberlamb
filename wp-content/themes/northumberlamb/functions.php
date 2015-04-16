5554cc32730349da170f6e74f3ecf8795032809f
w0172075 <w0172075@nscc.ca>
2015-04-16 08:38:07 -0300

    add logo

--- wp-content/themes/northumberlamb/functions.php
+++ wp-content/themes/northumberlamb/functions.php
@@ -1,11 +1,15 @@
 <?php
 define('JS_CSS_VER', '0.0.1');
 define('DESCRIPTION_LENGTH', 40);
-function quick_slug($s) { return sanitize_title(str_replace('/','-',$s)); }
+function quick_slug($s)
+{
+    return sanitize_title(str_replace('/', '-', $s));
+}
+
 // ------ ADDING STYLESHEETS ------
 $stylesheets = array(
 
-    'all'=>array(
+    'all' => array(
         'bootstrap-theme.css',
         'bootstrap-theme.css.map',
         'bootstrap-theme.min.css',
@@ -16,52 +20,54 @@ $stylesheets = array(
         'font-awesome.min.css',
     ),
 
-    'lt_ie_9'=>array(),
+    'lt_ie_9' => array(),
 
-    'gte_ie_9'=>array(),
+    'gte_ie_9' => array(),
 );
 add_action('wp_enqueue_scripts', 'add_stylesheets');
-function add_stylesheets() {
+function add_stylesheets()
+{
 
     global $wp_styles;
     global $stylesheets;
     $css = array();
 
-    foreach ( $stylesheets as $st ) {
-    foreach ( $st as $s ) {
+    foreach ($stylesheets as $st) {
+        foreach ($st as $s) {
 
-        $slug = quick_slug($s);
-        wp_enqueue_style($slug, get_bloginfo('stylesheet_directory').'/css/'.$s, $css, JS_CSS_VER);
-        array_push($css, $slug);
-    } }
+            $slug = quick_slug($s);
+            wp_enqueue_style($slug, get_bloginfo('stylesheet_directory') . '/css/' . $s, $css, JS_CSS_VER);
+            array_push($css, $slug);
+        }
+    }
 
     foreach ($stylesheets['lt_ie_9'] as $s) {
 
-        $wp_styles->add_data( quick_slug($s), 'conditional', 'lt IE 9' );
+        $wp_styles->add_data(quick_slug($s), 'conditional', 'lt IE 9');
     }
 
     foreach ($stylesheets['gte_ie_9'] as $s) {
 
-        $wp_styles->add_data( quick_slug($s), 'conditional', 'gte IE 9' );
+        $wp_styles->add_data(quick_slug($s), 'conditional', 'gte IE 9');
     }
 
 }
+
 // ------ ADDING JAVASCRIPTS ------
 $javascripts = array(
 
-    'head'=>array(
+    'head' => array(
 
-        'all'=>array(
-        ),
+        'all' => array(),
 
-        'lt_ie_9'=>array(),
+        'lt_ie_9' => array(),
 
-        'gte_ie_9'=>array(),
+        'gte_ie_9' => array(),
     ),
 
-    'body'=>array(
+    'body' => array(
 
-        'all'=>array(
+        'all' => array(
             'jquery.js',
             'bootstrap.js',
             'bootstrap.min.js',
@@ -73,51 +79,62 @@ $javascripts = array(
             'additional-methods.min.js',
         ),
 
-        'lt_ie_9'=>array(),
+        'lt_ie_9' => array(),
 
-        'gte_ie_9'=>array(),
+        'gte_ie_9' => array(),
     ),
 );
 add_action('wp_enqueue_scripts', 'add_javascripts');
-function add_javascripts(){
+function add_javascripts()
+{
 
     global $wp_scripts;
     global $javascripts;
     $js = array();
 
     foreach ($javascripts['head'] as $ja) {
-    foreach ($ja as $s) {
+        foreach ($ja as $s) {
 
-        $slug = quick_slug($s);
-        wp_enqueue_script($slug, get_bloginfo('stylesheet_directory').'/javascripts/'.$s, $js, JS_CSS_VER);
-        array_push($js, $slug);
-    } }
+            $slug = quick_slug($s);
+            wp_enqueue_script($slug, get_bloginfo('stylesheet_directory') . '/javascripts/' . $s, $js, JS_CSS_VER);
+            array_push($js, $slug);
+        }
+    }
 
     foreach ($javascripts['body'] as $ja) {
-    foreach ($ja as $s) {
+        foreach ($ja as $s) {
 
-        $slug = quick_slug($s);
-        wp_enqueue_script($slug, get_bloginfo('stylesheet_directory').'/javascripts/'.$s, $js, JS_CSS_VER, true);
-        array_push($js, $slug);
-    } }
-
-    foreach (array_merge($javascripts['head']['lt_ie_9'],$javascripts['body']['lt_ie_9']) as $s) {
-
-        $wp_scripts->add_data( quick_slug($s), 'conditional', 'lt IE 9' );
+            $slug = quick_slug($s);
+            wp_enqueue_script($slug, get_bloginfo('stylesheet_directory') . '/javascripts/' . $s, $js, JS_CSS_VER, true);
+            array_push($js, $slug);
+        }
     }
 
-    foreach (array_merge($javascripts['head']['gte_ie_9'],$javascripts['body']['gte_ie_9']) as $s) {
+    foreach (array_merge($javascripts['head']['lt_ie_9'], $javascripts['body']['lt_ie_9']) as $s) {
 
-        $wp_scripts->add_data( quick_slug($s), 'conditional', 'gte IE 9' );
+        $wp_scripts->add_data(quick_slug($s), 'conditional', 'lt IE 9');
+    }
+
+    foreach (array_merge($javascripts['head']['gte_ie_9'], $javascripts['body']['gte_ie_9']) as $s) {
+
+        $wp_scripts->add_data(quick_slug($s), 'conditional', 'gte IE 9');
     }
 }
-// ------ PRE FUNCTION FOR DEBUGGING ------
-function pre($a) { echo '<pre>'; print_r($a); echo '</pre>'; }
-// ------ GLOBAL SMARTY FUNCTION ------
-function wp_smarty(){
 
-     global $wp_smarty;
-    if(!$wp_smarty){
+// ------ PRE FUNCTION FOR DEBUGGING ------
+function pre($a)
+{
+    echo '<pre>';
+    print_r($a);
+    echo '</pre>';
+}
+
+// ------ GLOBAL SMARTY FUNCTION ------
+function wp_smarty()
+{
+
+    global $wp_smarty;
+    if (!$wp_smarty) {
 
         $wp_smarty = smarty_get_instance();
 
@@ -136,109 +153,121 @@ function wp_smarty(){
                 'menu_id' => 'header-menu'
             ));
 
-            $wp_smarty->assign('menu', $header_menu );
+            $wp_smarty->assign('menu', $header_menu);
 
             $wp_smarty->assign('title', $post->post_title);
-            $wp_smarty->assign('content', apply_filters('the_content', get_the_content() ));
+            $wp_smarty->assign('content', apply_filters('the_content', get_the_content()));
             //$wp_smarty->assign('here', here());
         }
     }
 
     return $wp_smarty;
 }
+
 // ------ MENUS ------
-add_action( 'init', 'create_menus' );
-function create_menus() {
-  register_nav_menus(
-    array(
-      'header-menu' => __( 'Header Menu' ),
-    )
-  );
+add_action('init', 'create_menus');
+function create_menus()
+{
+    register_nav_menus(
+        array(
+            'header-menu' => __('Header Menu'),
+        )
+    );
 }
+
 // ------ CUSTOM POST TYPES ------
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-    register_post_type( 'important-links',
+add_action('init', 'create_post_type');
+function create_post_type()
+{
+    register_post_type('important-links',
         array(
             'labels' => array(
                 'name' => 'Important Links',
                 'singular_name' => 'Important Link',
             ),
-        'public' => true,
-        'has_archive' => true,
+            'public' => true,
+            'has_archive' => true,
         )
     );
-    register_post_type( 'recipe',
+    register_post_type('recipe',
         array(
             'labels' => array(
                 'name' => 'Recipes',
                 'singular_name' => 'Recipe',
             ),
-        'public' => true,
-        'has_archive' => true,
+            'public' => true,
+            'has_archive' => true,
         )
     );
-     register_post_type( 'recipe-links',
+    register_post_type('recipe-links',
         array(
             'labels' => array(
                 'name' => 'Recipe Links',
                 'singular_name' => 'Recipe Link',
             ),
-        'public' => true,
-        'has_archive' => true,
+            'public' => true,
+            'has_archive' => true,
         )
     );
-    register_post_type( 'clients',
+    register_post_type('clients',
         array(
             'labels' => array(
                 'name' => 'Client',
                 'singular_name' => 'Clients',
             ),
-        'public' => true,
-        'has_archive' => true,
+            'public' => true,
+            'has_archive' => true,
         )
     );
-};
+}
+
+;
 // ------ IMAGE RESIZING ------
-add_image_size( 'banner', 640, 200, true );
-add_image_size( 'recipe-modal', 450, 200, true );
-add_image_size( 'recipe-list', 175, 125, true );
+add_image_size('banner', 640, 200, true);
+add_image_size('recipe-modal', 450, 200, true);
+add_image_size('recipe-list', 175, 125, true);
 // ------ /IMAGE RESIZING ------
 // ------ FETCH FUNCTIONS ------
-function fetch_page($page) {
+function fetch_page($page)
+{
 
-    $q = new WP_Query(array( 'post_type' => 'page', 'name' => $page ));
+    $q = new WP_Query(array('post_type' => 'page', 'name' => $page));
     $posts = $q->posts;
     return fetch($posts[0]);
 
 }
-function fetch($post) {
+
+function fetch($post)
+{
 
     setup_postdata($post);
 
-    $fields = array_merge( (array)$post, (array)get_fields($post->ID) );
+    $fields = array_merge((array)$post, (array)get_fields($post->ID));
 
     return $fields;
 }
 
 // ------ END FETCH FUNCTIONS ------
 // custom admin login logo
-function custom_login_logo() {
+function custom_login_logo()
+{
     echo '<style type="text/css">
     h1 a {
-        background-image: url('.get_bloginfo('template_directory').'/images/black-logo.png) !important;
+        background-image: url(' . get_bloginfo('template_directory') . '/images/black-logo.png) !important;
         width: 100% !important;
         background-size: 200px !important;
     }
     </style>';
 }
+
 add_action('login_head', 'custom_login_logo');
 
 //------Recipes
 add_action('wp_ajax_get_next_recipe', 'get_next_recipe');
 add_action('wp_ajax_nopriv_get_next_recipe', 'get_next_recipe');
 
-function get_next_recipe() {
+function get_next_recipe()
+{
 
     $id = $_POST['id'];
     //$next_id = $_POST['next_id'];
@@ -249,34 +278,38 @@ function get_next_recipe() {
     $posts = array();
     $next_post = false;
 
-    pre($id . " : " . $count );
+    pre($id . " : " . $count);
 
 }
 
-function load_custom_fonts($init) {
+function load_custom_fonts($init)
+{
 
     $stylesheet_url = 'custom-fonts/custom-fonts.css';
 
-    if(empty($init['content_css'])) {
+    if (empty($init['content_css'])) {
         $init['content_css'] = $stylesheet_url;
     } else {
-        $init['content_css'] = $init['content_css'].','.$stylesheet_url;
+        $init['content_css'] = $init['content_css'] . ',' . $stylesheet_url;
     }
 
     $font_formats = isset($init['font_formats']) ? $init['font_formats'] : 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
 
-    $custom_fonts = ';'.'Freestyle Script=freestyle script';
+    $custom_fonts = ';' . 'Freestyle Script=freestyle script';
 
     $init['font_formats'] = $font_formats . $custom_fonts;
 
     return $init;
 }
+
 add_filter('tiny_mce_before_init', 'load_custom_fonts');
 
-function load_custom_fonts_frontend() {
+function load_custom_fonts_frontend()
+{
 
     echo '<link type="text/css" rel="stylesheet" href="custom-fonts/custom-fonts.css">';
 }
+
 add_action('wp_head', 'load_custom_fonts_frontend');
 add_action('admin_head', 'load_custom_fonts_frontend');
 
--- wp-content/themes/northumberlamb/javascripts/northumberlamb.js
+++ wp-content/themes/northumberlamb/javascripts/northumberlamb.js
@@ -1,6 +1,6 @@
 var count = 0;
 
-$(function() {
+$(function () {
     // var btnPrint = $('#btnPrintRecipe');
     var btnPrint = document.getElementById("btnPrintRecipe");
 
@@ -8,95 +8,95 @@ $(function() {
     $('.carousel').carousel({
         interval: 5000 //changes the speed
     })
-    $('div#client_navigation h5 a').click(function(e){
+    $('div#client_navigation h5 a').click(function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $("#"+$(this).attr("id") +"_section").offset().top
+            scrollTop: $("#" + $(this).attr("id") + "_section").offset().top
         }, 2000);
     });
-    $('.overlay').click(function(){
-    	$(this).css('pointer-events', 'none');
+    $('.overlay').click(function () {
+        $(this).css('pointer-events', 'none');
     });
-    $('#btnShipperApplication').click(function(e){
+    $('#btnShipperApplication').click(function (e) {
         e.preventDefault();
         $('#shipper-application-modal').modal('show');
     });
-    $('#btnMemberApplication').click(function(e){
+    $('#btnMemberApplication').click(function (e) {
         e.preventDefault();
         $('#member-application-modal').modal('show');
     });
     // -----------------    RECIPE PAGE
-    $('#btnBroiling').click(function(e){
+    $('#btnBroiling').click(function (e) {
         e.preventDefault();
         $('#broiling-modal').modal('show');
     });
-    $('#btnBraising').click(function(e){
+    $('#btnBraising').click(function (e) {
         e.preventDefault();
         $('#braising-modal').modal('show');
     });
-    $('#btnRoasting').click(function(e){
+    $('#btnRoasting').click(function (e) {
         e.preventDefault();
         $('#roasting-modal').modal('show');
     });
-    $('a.recipe-modal-open').click(function(e){
+    $('a.recipe-modal-open').click(function (e) {
         e.preventDefault();
 
-        var url = '/ajax/?recipe_id='+ $(this).attr('rel');
+        var url = '/ajax/?recipe_id=' + $(this).attr('rel');
         count = (parseInt($(this).parent().attr('data-position')));
-        $.ajax( url ).done(function(response, status) {
+        $.ajax(url).done(function (response, status) {
             $('#recipe-popup .popup-content').html(response);
             $('#recipe-modal-ajax').modal('show');
-                $( "#recipe-modal-ajax" ).on('shown.bs.modal', function(e){
-                    $('#btnPrintRecipe').on('click', function(){
-                        printRecipe(document.getElementById("print-title"), false);
-                        printRecipe(document.getElementById("print-content"), true);
-                        window.print();
-                    });
-                    //disable buttons if first or last recipe is clicked
-                    if(count === 0){
-                        $('button#previous_recipe').prop('disabled', true);
-                    }else if(count === recipeData.length - 1){
-                        $('button#next_recipe').prop('disabled', true);
-                    }
-                    $('button#next_recipe').on('click', function() {
-                        e.preventDefault();
-                        increaseCount();
-                        populateModal(count);
-
-                        if(count === recipeData.length - 1){
-                            $('button#next_recipe').prop('disabled', true);
-                        }else if(count != 0){
-                            $('button#previous_recipe').prop('disabled', false);
-                        }
-
-                    });
-                    $('button#previous_recipe').on('click', function() {
-                        e.preventDefault();
-                        decreaseCount();
-                        populateModal(count);
-                        if(count === 0){
-                            $('button#previous_recipe').prop('disabled', true);
-                        }else if(count != recipeData.length - 1){
-                            $('button#next_recipe').prop('disabled', false);
-                        }
-                    });
+            $("#recipe-modal-ajax").on('shown.bs.modal', function (e) {
+                $('#btnPrintRecipe').on('click', function () {
+                    printRecipe(document.getElementById("print-title"), false);
+                    printRecipe(document.getElementById("print-content"), true);
+                    window.print();
                 });
+                //disable buttons if first or last recipe is clicked
+                if (count === 0) {
+                    $('button#previous_recipe').prop('disabled', true);
+                } else if (count === recipeData.length - 1) {
+                    $('button#next_recipe').prop('disabled', true);
+                }
+                $('button#next_recipe').on('click', function () {
+                    e.preventDefault();
+                    increaseCount();
+                    populateModal(count);
+
+                    if (count === recipeData.length - 1) {
+                        $('button#next_recipe').prop('disabled', true);
+                    } else if (count != 0) {
+                        $('button#previous_recipe').prop('disabled', false);
+                    }
+
+                });
+                $('button#previous_recipe').on('click', function () {
+                    e.preventDefault();
+                    decreaseCount();
+                    populateModal(count);
+                    if (count === 0) {
+                        $('button#previous_recipe').prop('disabled', true);
+                    } else if (count != recipeData.length - 1) {
+                        $('button#next_recipe').prop('disabled', false);
+                    }
+                });
+            });
         });
 
     });
-    function populateModal(count){
+    function populateModal(count) {
         //loop through cuts of lamb and append to string
         var cuts = "";
-        for(var i=0;i<= recipeData[count].lamb_cut.length-1;i++){
-            cuts += recipeData[count].lamb_cut[i] ;
-            if(i != recipeData[count].lamb_cut.length-1){
+        for (var i = 0; i <= recipeData[count].lamb_cut.length - 1; i++) {
+            cuts += recipeData[count].lamb_cut[i];
+            if (i != recipeData[count].lamb_cut.length - 1) {
                 cuts += ", ";
             }
         }
         //loop through ingredients of recipe and append to string
         $('ol#ul-recipe-ingredients').empty();
         var ingredients;
-        for(var i=0;i<= recipeData[count].ingredients.length-1;i++){
+        for (var i = 0; i <= recipeData[count].ingredients.length - 1; i++) {
             $('ol#ul-recipe-ingredients').append('<li>' + recipeData[count].ingredients[i].ingredient + '</li>');
         }
 
@@ -108,13 +108,16 @@ $(function() {
         $('div#print-content').find('p:nth-of-type(2)').fadeIn(600).html(recipeData[count].post_content);
         $('div#print-content').find('p:nth-of-type(5)').fadeIn(600).html(recipeData[count].instructions);
     }
-    function increaseCount(){
+
+    function increaseCount() {
         return count += 1;
     }
-    function decreaseCount(){
+
+    function decreaseCount() {
         return count -= 1;
     }
-        // ---- PRINTING MODAL
+
+    // ---- PRINTING MODAL
     function printRecipe(elem, append, delimiter) {
         var domClone = elem.cloneNode(true);
         var $printSection = document.getElementById("print-section");
@@ -126,7 +129,7 @@ $(function() {
         }
         if (append !== true) {
             $printSection.innerHTML = "";
-        }else if (append === true) {
+        } else if (append === true) {
             if (typeof(delimiter) === "string") {
                 $printSection.innerHTML += delimiter;
             }
@@ -137,70 +140,72 @@ $(function() {
 
         $printSection.appendChild(domClone);
     }
+
     // --- LAMB CUT CHART
     $('img[usemap]').rwdImageMaps();
 
-    $('#btnAllRecipes').on('click', function(){
+    $('#btnAllRecipes').on('click', function () {
         $('small.filtering').show();
         $('small.filtering span').html("All Recipes");
-            var target = '.recipe';
-            $(target).each(function() {
-                $(this).fadeIn( 200);
-            });
-            $('div.no-recipes').hide();
-            $('html, body').animate({
-                scrollTop: $(".div-recipes").offset().top
-            }, 1000);
+        var target = '.recipe';
+        $(target).each(function () {
+            $(this).fadeIn(200);
+        });
+        $('div.no-recipes').hide();
+        $('html, body').animate({
+            scrollTop: $(".div-recipes").offset().top
+        }, 1000);
     });
 
-    $('area').on('click', function(e) {
-             e.preventDefault();
-            var filter = $(this).attr('alt');
-            $('small.filtering span').html(filter);
-            var matched = 0;
+    $('area').on('click', function (e) {
+        e.preventDefault();
+        var filter = $(this).attr('alt');
+        $('small.filtering span').html(filter);
+        var matched = 0;
 
-            var target = '.recipe';
-            $(target).each(function() {
-                $(this).hide();
-            });
-            //Lamb Cut Chart Filtering
-            $(target).each(function() {
-                var item = this;
-                var lamb_cuts = $(item).attr('data-cuts').split(" ");
+        var target = '.recipe';
+        $(target).each(function () {
+            $(this).hide();
+        });
+        //Lamb Cut Chart Filtering
+        $(target).each(function () {
+            var item = this;
+            var lamb_cuts = $(item).attr('data-cuts').split(" ");
 
-                $.each(lamb_cuts, function(k,v){
-                    //if the category matches the filter then display that div
-                    if ( v === filter ) {
-                        matched ++;
-                        $(item).fadeIn( 200);
-                    }
-                });
+            $.each(lamb_cuts, function (k, v) {
+                //if the category matches the filter then display that div
+                if (v === filter) {
+                    matched++;
+                    $(item).fadeIn(200);
+                }
             });
-            if(matched === 0){
-                $('div.no-recipes').fadeIn( 200);;
-                $('small.filtering').hide();
-            }else{
-                $('div.no-recipes').hide();
-                $('small.filtering').show();
-            }
-            $('html, body').animate({
-                scrollTop: $(".div-recipes").offset().top
-            }, 1000);
+        });
+        if (matched === 0) {
+            $('div.no-recipes').fadeIn(200);
+            ;
+            $('small.filtering').hide();
+        } else {
+            $('div.no-recipes').hide();
+            $('small.filtering').show();
+        }
+        $('html, body').animate({
+            scrollTop: $(".div-recipes").offset().top
+        }, 1000);
     });
     // END RECIPE PAGE MODAL
-    $('ul.nav-tabs li a').click(function(e){
-    	e.preventDefault();
+    $('ul.nav-tabs li a').click(function (e) {
+        e.preventDefault();
     });
 // -------------------------------------------------------------------------- dropdown menu for important links
     $('li > a:contains("Important Links")').addClass("dropdown-toggle");
-    $('li > a:contains("Important Links")').attr('data-toggle','dropdown');
+    $('li > a:contains("Important Links")').attr('data-toggle', 'dropdown');
     $('li > a:contains("Important Links")').next().addClass("dropdown-menu");
-    $('li > a:contains("Important Links")').next().attr('role','menu');
-    $('li > a:contains("Important Links")').next().find('a').attr('target','_blank');
+    $('li > a:contains("Important Links")').next().attr('role', 'menu');
+    $('li > a:contains("Important Links")').next().find('a').attr('target', '_blank');
     $('.dropdown-toggle').dropdown();
 
     //----------------------------------------------------------------------- form validation
-    jQuery.validator.addMethod("cdnPostal", function(postal, element) {
+    jQuery.validator.addMethod("cdnPostal", function (postal, element) {
         return this.optional(element) ||
             postal.match(/[a-zA-Z][0-9][a-zA-Z](-| |)[0-9][a-zA-Z][0-9]/);
     }, "Please specify a valid postal code.");
